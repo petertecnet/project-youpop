6a6e5e3a6ca2b37053910b6e66fba411af4e2314
petertecnet <pivow@hotmail.com>
2023-05-10 21:22:48 -0300

    Creating youpop, youpop_lojista, youpop_entregador app

---
+++ youpop/android/app/src/main/kotlin/com/example/youpop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.youpop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ youpop_entregador/android/app/src/main/kotlin/com/example/youpop_entregador/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.youpop_entregador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
