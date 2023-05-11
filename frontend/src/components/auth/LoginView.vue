48a5996b0a45250b4d7d8525d148a18ce150f076
petertecnet <pivow@hotmail.com>
2023-05-11 17:41:26 -0300

    Login scream until now

--- frontend/src/components/auth/LoginView.vue
+++ frontend/src/components/auth/LoginView.vue
@@ -1,31 +1,120 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <input type="email" v-model="email" placeholder="Email">
-      <input type="password" v-model="password" placeholder="Senha">
-      <button type="submit">Entrar</button>
-    </form>
+  <div class="login-container">
+    <div class="login-card">
+      <h2 class="login-title">Bem-vindo ao YouPop</h2>
+      <form class="login-form" @submit.prevent="login">
+        <input type="email" v-model="email" class="login-input" placeholder="E-mail" required>
+        <input type="password" v-model="password" class="login-input" placeholder="Senha" required>
+        <div class="login-actions">
+          <label class="remember-checkbox">
+            <input type="checkbox" v-model="remember"> Lembrar senha
+          </label>
+          <a href="#" class="forgot-password">Esqueceu a senha?</a>
+        </div>
+        <button type="submit" class="login-button">Entrar</button>
+      </form>
+      <div class="login-options">
+        <p class="signup-link">Ainda não tem uma conta? <a href="#" class="signup">Cadastre-se</a></p>
+        <p class="signup-link">É entregador? <a href="#" class="signup">Cadastre-se como entregador</a></p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginView', // ou 'LoginForm' ou outro nome composto
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      remember: false,
     };
   },
   methods: {
-    async login() {
-      // Método de login atualizado
-    }
-  }
+    login() {
+      // Implemente a lógica de autenticação aqui
+      console.log('Efetuando login...');
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
 /* Estilos do componente */
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f06432;
+}
+
+.login-card {
+  width: 500px;
+  padding: 50px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.login-title {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  color: #3f51b5;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+.login-input {
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #f5f5f5;
+  color: #333;
+}
+
+.login-actions {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+
+.remember-checkbox {
+  font-size: 14px;
+}
+
+.forgot-password {
+  font-size: 14px;
+  color: #999;
+  text-decoration: none;
+}
+
+.login-button {
+  padding: 10px;
+  background-color: #3f51b5;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.login-options {
+  text-align: center;
+}
+
+.signup-link {
+  margin-bottom: 10px;
+}
+
+.signup-link a {
+  color: #3f51b5;
+  text-decoration: none;
+}
 </style>
